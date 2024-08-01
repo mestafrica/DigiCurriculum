1be62acaacf0619da217c8d2bb6ca0c472a3a401
Atinadcodes <lamptey.danita@gmail.com>
2024-08-01 21:01:34 +0000

    refactoring of route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/signup", element: <Signup /> },
+];
+
 function App() {
   return (
-    <>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-    </Routes>
-    
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </BrowserRouter>
-     
-      
-    </>
   );
 }
 
