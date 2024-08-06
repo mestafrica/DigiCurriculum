241ccb325981a4b34e276be41abdc3489d0a6d83
unknown <maamesikadanquah203@gmail.com>
2024-08-06 17:48:42 +0000

    page layout update 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import PageLayout from "@/components/PageLayout/pageLayout";
 import Hero from "@/components/ui/ui/Hero";
 
 
 function Home() {
   return (
     <>
+    <PageLayout>
     <Hero/>
+    </PageLayout>
     </>
   )
 }
