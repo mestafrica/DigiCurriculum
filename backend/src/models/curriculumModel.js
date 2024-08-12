f92ba640195b9d02154c757227e109b3a01e0059
lyril-sly <sylvialyril.sl@gmail.com>
2024-08-12 21:28:08 +0000

    changes made to the cirriculum

--- backend/src/models/curriculumModel.js
+++ backend/src/models/curriculumModel.js
@@ -4,14 +4,17 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 // PerformanceIndicator Schema
-const performanceIndicatorSchema = new Schema({
+const performanceIndicatorSchema = new mongoose.Schema({
   code: {type: String, required: true, unique: true},
   name: {type: String,required: true},
   description: { type: String },
+  suggestedTimeframe: {type: String},
+  assessmentCriteria: {type: String},
+  resources: {type: String}
 });
 
 // Schema for SubStrand
-const subStrandSchema = new Schema({
+const subStrandSchema = new mongoose.Schema({
   code: { type: String, required: true,unique: true },
   name: { type: String, required: true},
   description: { type: String },
@@ -20,26 +23,25 @@ const subStrandSchema = new Schema({
 });
 
 // Schema for Strand 
-const strandSchema = new Schema({
+const strandSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
-  name: {type: String, required: true 
-    },
+  name: {type: String, required: true},
   description: { type: String },
   subStrand: [subStrandSchema]
   
 });
 
 // schema for Course
-const courseSchema = new Schema({
-  code: {type: String, required: true, unique: true },
-  name: { type: String,required: true},
+const courseSchema = new mongoose.Schema({
+  courseCode: {type: String, required: true, unique: true },
+  courseName: { type: String,required: true},
   description: { type: String },
   strands: [strandSchema],
 });
 
 // Schema for Curriculum
-const curriculumSchema = new Schema({
-  code: { type: String, required: true, unique: true },
+const curriculumSchema = new mongoose.Schema({
+  code: { type: String, required: true, unique: true, min: 1 },
   name: { type: String, required: true },
   description: { type: String },
   timeframes: { type: String },
