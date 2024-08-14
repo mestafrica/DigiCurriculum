c55b7bd97da00e67e49cc9555faf1ba5ab0e0d30
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-08-14 23:29:24 +0000

    Lesson plan controllers and models

---
+++ backend/src/routes/lessonPlanRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  generateLessonPlan,
+  getLessonPlans,
+  updateLessonPlan,
+  deleteLessonPlan,
+} from "../controllers/lessonPlanController.js";
+
+const router = express.Router();
+
+router.post("/generate/lessonplan", generateLessonPlan);
+router.get("/lessonplans", getLessonPlans);
+router.patch("/lessonplans/:id", updateLessonPlan);
+router.delete("/lessonplans/:id", deleteLessonPlan);
+
+export default router;
