9e9bba40d75e085fa95aa7c590442b6c35ac0c8a
joanaquadzie <joanaq03@gmail.com>
2024-08-15 16:31:21 +0000

    completed the about section

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Underline from "../assets/images/underline.png";
-import Photo1 from "../assets/images/photo1.png";
-import Photo2 from "../assets/images/photo2.png";
-import Photo3 from "../assets/images/photo3.png";
 import Content1 from "../assets/images/content1.png";
 import Content2 from "../assets/images/content2.png";
 import Frame1 from "../assets/images/frame1.png";
 import Frame2 from "../assets/images/frame2.png";
 import Frame3 from "../assets/images/frame3.png";
+import Vector from "../assets/images/vector.jpg";
+import Vector2 from "../assets/images/vector2.jpg";
+import Vector3 from "../assets/images/vector3.jpg";
 
 export default function About() {
   return (
@@ -22,7 +22,7 @@ export default function About() {
       >
         <div className="flex flex-col items-center py-44">
           <h1 className="text-6xl text-white font-bold font-serif">About Us</h1>
-          <p className="text-white text-lg font-semibold md:w-2/4 p-10 pt-10">
+          <p className="text-white text-center text-lg font-semibold md:w-2/4 p-10 pt-10">
             Our online curriculum offers dynamic and interactive lessons for
             students. We ensure that learning is enjoyable and meaningful,
             inspiring students to explore and think critically beyond the
@@ -45,15 +45,18 @@ export default function About() {
           <img src={Underline} />
         </div>
         <div className="flex flex-col justify-center md:flex-row md:mt-14 md:gap-24">
-          <img className="w-64 md:w-96 ml-10" src={Photo1} />
+          <img className="w-64 md:w-96 ml-10" src={Vector} />
           <img className="p-6" src={Content1} />
         </div>
         <div className="flex flex-col justify-center md:flex-row md:mt-24 md:gap-24">
-          <img className="ml-10" src={Content2} />
-          <img className="p-6 w-72 ml-10 md:w-96" src={Photo2} />
+          <img className="ml-10 order-2 md:order-1" src={Content2} />
+          <img
+            className="p-6 w-72 ml-10 md:w-96 order-1 md:order-2"
+            src={Vector2}
+          />
         </div>
         <div className="flex flex-col justify-center md:flex-row md:mt-24 md:gap-24">
-          <img className="w-64 md:w-96 ml-10" src={Photo3} />
+          <img className="w-64 md:w-96 mt-8 ml-10" src={Vector3} />
           <img className="p-6" src={Content1} />
         </div>
         <div className="flex flex-col items-center">
