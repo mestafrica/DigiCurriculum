88018a7020f3c8ab93b573201ca952114c10ad84
emmanuelAkpe <emmanuelakpe1@gmail.com>
2024-11-07 11:57:53 +0000

    updated errors in the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,24 @@
-
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
-import AccountRouter from "./src/routes/usersRoutes.js"
-import calendarRouter from "./src/routes/calendarRoutes.js"
-// import curriculumRoutes from "./src/routes/curriculumRoutes.js"
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './src/utils/swagger.js'
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import adminRouter from "./src/routes/adminRoutes.js";
-
-
-
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import AccountRouter from "./src/routes/usersRoutes.js";
 import calendarRouter from "./src/routes/calendarRoutes.js";
-import curriculumRoutes from "./src/routes/curriculumRoutes.js";
-import assessmentRoutes from "./src/routes/assessmentRoutes.js";
-import lessonRoutes from "./src/routes/lessonPlanRoutes.js";
+// import curriculumRoutes from "./src/routes/curriculumRoutes.js"
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./src/utils/swagger.js";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import adminRouter from "./src/routes/adminRoutes.js";
+
+import curriculumRoutes from "./src/routes/curriculumRoutes.js";
+import assessmentRoutes from "./src/routes/assessmentRoutes.js";
+import lessonRoutes from "./src/routes/lessonPlanRoutes.js";
 
 dotenv.config();
 const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
 app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URI }));
@@ -56,15 +43,12 @@ mongoose
   })
   .catch((error) => console.log(error));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use(AccountRouter)
-app.use(calendarRouter)
+app.use(AccountRouter);
+app.use(calendarRouter);
 // app.use(curriculumRoutes)
-app.use(adminRouter)
-
-
+app.use(adminRouter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
@@ -75,5 +59,5 @@ app.use(assessmentRoutes);
 app.use(lessonRoutes);
 
 app.listen(PORT, () => {
-    console.log(`The server is running! on ${PORT}`)
+  console.log(`The server is running! on ${PORT}`);
 });
