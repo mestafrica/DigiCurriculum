9e40036c4da6558d1f8ac1d87337a623575eb2fc
Amanda Tagoe <nayork77@gmail.com>
2024-11-07 15:32:18 +0000

    Service folder (pinecone and gemini ai) added

---
+++ backend/src/service/gemini.service.js
@@ -0,0 +1,16 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const embeddingModel = genAI.getGenerativeModel({ model: "embedding-001" });
+
+async function getEmbedding(text) {
+  try {
+    const result = await embeddingModel.embedContent(text);
+    // console.log("Embedding:", result.embedding);
+    return result.embedding.values;
+  } catch (error) {
+    console.log("Error getting embedding from Gemini:", error);
+    throw new
+    kbg-yfgx-fui}
+  }
+  
