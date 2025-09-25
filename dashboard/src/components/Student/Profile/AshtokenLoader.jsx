c538640ce7b86818e8e314b47aa6d7af407d2aed
stuck in code <bonidaniel52@gmail.com>
2025-09-25 16:32:57 +0000

    added the functionalities to fecth user data and also edit the data when needed

---
+++ dashboard/src/components/Student/Profile/AshtokenLoader.jsx
@@ -0,0 +1,9 @@
+function AshtokenLoader() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-6 h-6 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default AshtokenLoader;
--- dashboard/src/components/Student/Profile/profileDetailsAdmin.jsx
+++ dashboard/src/components/Student/Profile/profileDetailsAdmin.jsx
@@ -5,6 +5,8 @@ import EditProfile from "./editProfile";
 import EditPassword from "./passwordEdit";
 import axios from "axios";
 import { token } from "../../../../config";
+//import { jwtDecode } from "jwt-decode";
+//import { useParams } from "react-router-dom";
 
 const ProfileDetailStudent = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -20,31 +22,40 @@ const ProfileDetailStudent = () => {
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
-    // setSelectedFile(event.target.files[0]);
   };
-
+  //const { id } = useParams();
   useEffect(() => {
     getUserData();
   }, []);
 
-  const getUserData = () => {
-    const myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json");
-    myHeaders.append("Authorization", `Bearer ${token}`);
+  const getUserData = async () => {
+    try {
+      // const decoded = jwtDecode(token);
+      // console.log(decoded);
+      // const userId = decoded?.id || decoded?.userId || decoded?.sub;
 
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
+      // if (!userId) {
+      //   console.error("❌ User ID not found in token");
+      //   return;
+      // }
 
-    fetch("http://.....217/api/user", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        setUserData(result);
-        console.log(result);
-      })
-      .catch((error) => console.error(error));
+      const id = "68c2d63504c670a41f02403b";
+      const res = await axios.get(
+        `https://digicurriculum.onrender.com/user/${id}`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Use "userz" instead of "user"
+      setUserData(res.data.user);
+      console.log("User data fetched:", res.data.user);
+    } catch (err) {
+      console.error("Error fetching user data:", err);
+    }
   };
 
   const handleImageChange = (event) => {
@@ -98,14 +109,14 @@ const ProfileDetailStudent = () => {
       <div className="flex flex-wrap justify-center p-8">
         <div className="w-full h-full lg:w-1/3 p-4 backdrop-blur-sm bg-black/10 border border-[#A7D7C5] rounded-lg">
           <div className="text-center">
-            <h2 className="text-2xl text-primary font-bold">{userData.name}</h2>
+            <h2 className="text-2xl text-primary font-bold">
+              {userData.firstName} {userData.lastName}
+            </h2>
           </div>
           <div className="my-4 flex justify-center items-center">
             <img
               src={
-                userData
-                  ? `http://.....217/${userData.avatar}`
-                  : selectedImage
+                userData ? `http://.....217/${userData.avatar}` : selectedImage
               }
               alt="Profile Image"
               className="rounded-full w-44 h-44"
@@ -118,20 +129,20 @@ const ProfileDetailStudent = () => {
               onChange={handleImageChange}
               className="hidden"
             />
-           <div className="flex flex-col gap-4">
-           <button
-              onClick={handleButtonClick}
-              className="bg-gray-600 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded"
-            >
-              Select Image
-            </button>
-            <button
-              onClick={handleUpload}
-              className="bg-secondary hover:bg-primary text-black font-bold py-2 px-4 rounded"
-            >
-              Update Avatar
-            </button>
-           </div>
+            <div className="flex flex-col gap-4">
+              <button
+                onClick={handleButtonClick}
+                className="bg-gray-600 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded"
+              >
+                Select Image
+              </button>
+              <button
+                onClick={handleUpload}
+                className="bg-secondary hover:bg-primary text-black font-bold py-2 px-4 rounded"
+              >
+                Update Avatar
+              </button>
+            </div>
             <p className="text-sm text-zinc-500 mt-2">
               Upload a new avatar. Larger image will be resized automatically.
               Maximum upload size is 1 MB
@@ -144,9 +155,11 @@ const ProfileDetailStudent = () => {
         <div className="w-full lg:w-2/3 px-4">
           <div className="backdrop-blur-sm bg-black/10 border border-[#A7D7C5] shadow-lg rounded-lg p-4">
             <div className="flex justify-between border-b pb-2">
-              <h2 className="text-xl font-bold text-gray-800">Edit Profile</h2>
+              <h2 className="text-xl font-bold text-gray-800">Your Profile</h2>
               <div className="flex">
-                <button className="text-primary font-semibold">User info</button>
+                <button className="text-primary font-semibold">
+                  User info
+                </button>
               </div>
             </div>
             <div className="flex w-full flex-col gap-4 mt-4">
@@ -156,7 +169,7 @@ const ProfileDetailStudent = () => {
                     Full Name
                   </label>
                   <span className=" rounded w-full  text-[#9399A6] leading-tight">
-                    {userData.name}
+                    {userData.firstName} {userData.lastName}
                   </span>
                 </div>
                 <button
@@ -170,21 +183,10 @@ const ProfileDetailStudent = () => {
                 <div className="flex justify-between gap-4">
                   <div className="w-full">
                     <label className="block text-black text-sm font-bold mb-2">
-                      Phone
+                      School
                     </label>
                     <span className=" rounded w-full  text-[#9399A6] leading-tight">
-                      {userData.phone}
-                    </span>
-                  </div>
-                  <div className="w-full">
-                    <label
-                      htmlFor="genderOptions"
-                      className="block text-black text-sm font-bold mb-2"
-                    >
-                      Gender
-                    </label>
-                    <span className=" rounded w-full  text-[#9399A6] leading-tight">
-                      {userData.gender}
+                      {userData.school}
                     </span>
                   </div>
                 </div>
@@ -214,7 +216,7 @@ const ProfileDetailStudent = () => {
                   Password
                 </label>
                 <span className=" rounded w-full  text-[#9399A6] leading-tight">
-                  {"••••••••••••••••••"}
+                  {userData._id}
                 </span>
               </div>
               <button
